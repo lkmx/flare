c7b0355d16b11cbcc757737052f2515845d9acb2
Walter Hurtado <walter.hurtado@lkmx.io>
2021-06-11 13:35:35 -0700

    - Added prop "revealer" to block element

--- src/core/Block.vue
+++ src/core/Block.vue
@@ -1,11 +1,41 @@
 <template>
-  <div class="--flare --flare-block">
-    <div class="content"><div class="box"><slot></slot></div></div>
+  <div class="--flare --flare-block" :class="revealer ? `${this.rev}` : ''">
+    <div class="content">
+      <div class="box">
+        <slot></slot>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: "Block",
-};
+  export default {
+    name: "Block",
+    props: {
+      revealer: {
+        type: Boolean,
+        require: false,
+      }
+    },
+    data: () => ({
+      observer: null,
+      rev: '--fl-revealer',
+    }),
+    mounted() {
+      this.observer = new IntersectionObserver(([entry]) => {
+        let el = this.$el;
+        if (el.classList.contains(this.rev) && entry.isIntersecting) {
+          this.$emit("intersect");
+          el.classList.add('active');
+        } else {
+          el.classList.remove('active');
+        }
+      });
+      this.observer.observe(this.$el);
+    },
+    destroyed() {
+      this.observer.disconnect();
+    },
+  };
+
 </script>
